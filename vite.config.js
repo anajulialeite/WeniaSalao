9bb3cfc8499c75931cf48734f8783e337286485b
Ana Júlia Leite <leite.anajulia25@gmail.com>
2026-05-10 18:50:14 -0300

    Atualização
    Base path /WeniaSalao/

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/WeniaSalao/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: false,
   },
   server: {
     host: true, // Allows access from mobile on same network
